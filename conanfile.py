358bb87a2b4808cbdf02778cfb8aa9700bac60b5
Lukasz Laszko <lukasz@fairtide.co>
2019-03-07 18:20:55 +0800

    initial checkin

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conans import ConanFile, CMake, tools
+
+
+class SbeConan(ConanFile):
+    name = "sbe"
+    version = "1.12.2"
+    license = "Apache License, Version 2.0"
+    url = "https://github.com/real-logic/simple-binary-encoding"
+    description = "Simple Binary Encoding (SBE) - High Performance Message Codec"
+    topics = ("codec", "java", "c-plus-plus", "encoder-decoder")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def source(self):
+        git = tools.Git(folder="simple-binary-encoding")
+        git.clone("https://github.com/real-logic/simple-binary-encoding.git", SbeConan.version)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="simple-binary-encoding")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include/otf", src="simple-binary-encoding/sbe-tool/src/main/cpp/otf")
+        self.copy("*.jar", dst="bin", src="simple-binary-encoding/sbe-tool/build", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["simple-binary-encoding"]
