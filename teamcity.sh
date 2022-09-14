b0875cbfdd45affecd3fbc1f2b7129c43401e955
Antoine Waugh <antoine@relativetechnologies.com>
2022-09-14 13:27:21 +1000

    Remove reference to prod profile

--- teamcity.sh
+++ teamcity.sh
@@ -2,7 +2,7 @@
 
 image=092165354856.dkr.ecr.ap-southeast-2.amazonaws.com/symbio-devtools:latest
 
-eval $(aws ecr get-login --profile=prod --region ap-southeast-2 | sed -e 's/-e none//g')
+eval $(aws ecr get-login --region ap-southeast-2 | sed -e 's/-e none//g')
 
 docker pull ${image}
 
