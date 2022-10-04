51f0b6212661d0f6db4976e4c9e8ff68f9c13cc4
Antoine Waugh <antoine@relativetechnologies.com>
2022-10-04 22:00:59 +1100

    Check package integrity prior to uploading, only upload new packages

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-conan create . --build=missing -pr:b=default -pr:h=default
-conan upload -c "*" --all --remote staconan --force
+conan create . --build=missing && \
+  conan upload -c "*" --confirm --all --remote staconan
